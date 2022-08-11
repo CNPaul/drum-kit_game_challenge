a219a92361e388364a4f76c61c3590a3e0feba41
CNPaul <chinedungene3@gmail.com>
2022-08-11 22:45:45 +0100

    Added different sounds to all the buttons using JavaScript Switch expression.🤓

--- index.js
+++ index.js
@@ -2,7 +2,41 @@ let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    let audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
+    let buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+      case "w":
+        let tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "a":
+        let tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "s":
+        let tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "d":
+        let tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "j":
+        let snareSound = new Audio("sounds/snare.mp3");
+        snareSound.play();
+        break;
+      case "k":
+        let crashSound = new Audio("sounds/crash.mp3");
+        crashSound.play();
+        break;
+      case "l":
+        let kickBass = new Audio("sounds/kick-bass.mp3");
+        kickBass.play();
+        break;
+
+      default:
+        console.log(buttonInnerHTML);
+        break;
+    }
   });
 }
